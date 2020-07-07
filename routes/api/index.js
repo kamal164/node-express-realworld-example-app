3f11d2ba98ef6337ce9ba2bdeb675bea98c5f704
kamal fatihi <mohammedkamal.fatihi@gmail.com>
2020-07-07 03:32:52 +0200

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,7 +6,7 @@ router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
 
 router.use(function(err, req, res, next){
-  if(err.name === 'ValidationError'){
+  if(err.name === 'ValidationError'){ 
     return res.status(422).json({
       errors: Object.keys(err.errors).reduce(function(errors, key){
         errors[key] = err.errors[key].message;
